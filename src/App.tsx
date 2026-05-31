8b3abb418423221004ba026d717a0d1baad8f932
S-276 <134470512+S-276@users.noreply.github.com>
2026-05-31 19:48:45 +0100

    Rename JobCard import to JobCards

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import JobCard from "./components/JobCard";
+import JobCard from "./components/JobCards";
 import JobForm from "./components/JobForm";
 import { Job, WeekEntry } from "./types";
 
