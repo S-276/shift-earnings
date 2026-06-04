95bc0d845cda89be98220cbfbf8e675393ad238a
S-276 <134470512+S-276@users.noreply.github.com>
2026-06-04 15:16:55 +0100

    Delete src/utils/shifts.ts

--- src/utils/shifts.ts
+++
@@ -1,9 +0,0 @@
-import { Shift } from "../types";
-
-export function calculateHours(shift: Shift): number {
-  const start = new Date(`2000-01-01T${shift.start}`);
-  const end = new Date(`2000-01-01T${shift.end}`);
-
-  const minutes = (end.getTime() - start.getTime()) / 60000;
-  return (minutes - shift.breakMinutes) / 60;
-}
