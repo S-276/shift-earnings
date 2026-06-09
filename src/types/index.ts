0f92381bbf697ae372009404f1105106c0bacbbe
S-276 <134470512+S-276@users.noreply.github.com>
2026-06-09 18:09:09 +0100

    Add PayePeriodType and update interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,8 @@ export type PayCycleType = "variable" | "fixed";
 
 export type NiPeriodType = "monthly" | "pay-period-weeks";
 
+export type PayePeriodType = "monthly" | "weekly";
+
 export interface PayPeriod {
   id: string;
   label: string;
@@ -16,12 +18,19 @@ export interface Job {
   taxCode: string;
   hourlyRate: number;
   hoursWorked: number;
+
   payCycleType: PayCycleType;
   niPeriodType: NiPeriodType;
+  payePeriodType: PayePeriodType;
+
+  includeHolidayPay: boolean;
+  holidayPayRate: number;
+
   selectedPayPeriodId?: string;
   customStartDate?: string;
   customEndDate?: string;
   customPayday?: string;
+
   previousGrossYTD: number;
   previousTaxPaidYTD: number;
 }
@@ -30,10 +39,16 @@ export interface JobResult {
   jobId: string;
   name: string;
   taxCode: string;
+
+  basicGross: number;
+  holidayPay: number;
   gross: number;
+
   tax: number;
   ni: number;
   net: number;
-  taxMonthNumber: number;
+
+  taxPeriodNumber: number;
+  taxPeriodType: PayePeriodType;
   payday: string;
 }
